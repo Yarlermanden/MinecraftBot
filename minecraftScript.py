1563256a2d535bafd2b0ce443b3bb53250d829b0
Yarlermanden <mmho@itu.dk>
2021-04-01 15:30:43 +0200

    Can actually control the mouse in Minecraft on Linux, however the keyboard controls doesn't work in minecraft

--- minecraftScript.py
+++ minecraftScript.py
@@ -1,4 +1,5 @@
-from pynput.keyboard import Key, Controller
+from pynput.keyboard import Key, Controller, Listener
+import keyboard as kkeyboard
 from pynput import mouse as Mouse
 import pynput
 import time
@@ -7,32 +8,35 @@ def moveMinecraftPlayer():
     while(True):
         keyboard.press('w')
         time.sleep(1)
-        keyboard.release('w') 
+        keyboard.release('w')
 
         keyboard.press('s')
         time.sleep(1)
-        keyboard.release('s') 
+        keyboard.release('s')
 
-def mineCobblestone():
+def strip_mine():
     i = 0
     while(True):
-        keyboard.press('w')
-        time.sleep(1)
-        keyboard.release('w')
+        #keyboard.press('w')
+        #time.sleep(1)
+        #keyboard.release('w')
 
-        #mouse.move(200, 500)
         #mouse.click(Mouse.Button.left,1)
-        position = mouse.position
-        
-        #mouse._position_set((position[0]+50, position[1]+100))
+        mouse.move(0, -200)
         mouse.press(Mouse.Button.left)
         time.sleep(1.5)
         mouse.release(Mouse.Button.left)
         time.sleep(0.2)
-        if i%20 == 0:
-            mouse.click(Mouse.Button.right,1)
+        
+        mouse.move(0, 200)
+        mouse.press(Mouse.Button.left)
+        time.sleep(1.5)
+        mouse.release(Mouse.Button.left)
+        time.sleep(0.2)
 
-        i = i+1
+        #if i%20 == 0:
+            #mouse.click(Mouse.Button.right,1)
+        #i = i+1
 
 def move_smooth(xm, ym, t):
     for i in range(t):
@@ -57,11 +61,23 @@ def move_mouse():
         #mouse.scroll(1,1)
         #mouse._position_set((position[0]+50, position[1]+100))
 
-
+def move_mouse_with_keyboard():
+    while(True):
+        time.sleep(0.01)
+        if kkeyboard.is_pressed('k'):
+            mouse.move(0, -10)
+        elif kkeyboard.is_pressed('j'):
+            mouse.move(0, 10)
+        elif kkeyboard.is_pressed('h'):
+            mouse.move(-10, 0)
+        elif kkeyboard.is_pressed('l'):
+            mouse.move(10, 0)
 
 mouse = Mouse.Controller()
 keyboard = Controller()
+listener = Listener()
 time.sleep(5)
-move_mouse()
-#mineCobblestone()
+#move_mouse_with_keyboard()
+#move_mouse()
+strip_mine()
 #moveMinecraftPlayer()
