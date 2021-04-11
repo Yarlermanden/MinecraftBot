5f2c18bfacea8824873d3f4bdfcbdc251fb2a64c
MMHO <MMHO@itu.dk>
2021-04-11 23:40:28 +0200

    Added a mode for linux to show what the bot sees through cv2.imshow

--- MineRL/env.py
+++ MineRL/env.py
@@ -1,21 +1,33 @@
 import gym
 import minerl
-import logging
-
+import PIL
+import cv2
+#import logging
 #logging.basicConfig(level=logging.DEBUG)
 
-env = gym.make('MineRLNavigateDense-v0')
-#env.make_interactive(port=6666, realtime=True)
+linux = True
 
-obs = env.reset()
+env = gym.make('MineRLNavigateDense-v0')
+env.STEP_OPTIONS=300
+#env.seed(420)
+#env.make_interactive(port=6666, realtime=True)
+env.reset()
+
 net_reward = 0
-#env.render()
 
 done = False
 while not done:
+    if linux:
+        img = env.render('rgb_array')
+        img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+        cv2.imshow('h', img)
+        cv2.waitKey(1)
+    else:
+        env.render()
     action = env.action_space.sample()
     #print('action: ' + str(action))
 
     obs,reward,done,info = env.step(action)
     net_reward += reward
     print("Total reward: ", net_reward)
+
